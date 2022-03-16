2d73ec9e373524327ce726897f98703d04dda58e
Mitch Chanza <mitch.chanza@gmail.com>
2022-03-16 23:59:33 +0200

    Include inportant files

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.additions = void 0;
+const additions = (adds) => {
+    return adds.a + adds.b;
+};
+exports.additions = additions;
+(0, exports.additions)({ a: 6, b: 7 });
