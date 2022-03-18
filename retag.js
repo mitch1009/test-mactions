d9194a224293fbb74d143193e1e19a1da6498a08
Mitch Chanza <mitch.chanza@gmail.com>
2022-03-18 02:26:41 +0200

    to

--- retag.js
+++ retag.js
@@ -4,5 +4,7 @@ const {exec} = require('child_process')
 exec(`npm version patch -m "Upgrade to %s from $npm_package_version "`, ((error, stdout, stderr) => {
     if(error) console.log(`stdout:${error}`)
     if(stderr) console.log(`stderr: ${stderr}`)
-    console.log("increased version", newVersion)
+    console.log("increased version", stdout)
 }))
+
+console.log("done")
